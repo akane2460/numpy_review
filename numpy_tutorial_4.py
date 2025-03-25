8fe17c6c550a27f448ad6a462cab245ad78e1b26
akane2460 <allisonkane2024@u.northwestern.edu>
2025-03-24 21:11:56 -0500

    numpy tutorial 4 view and copy arrays work

--- numpy_tutorial_4.py
+++ numpy_tutorial_4.py
@@ -6,8 +6,18 @@ import numpy as np
 # Copy
 array = np.array([1, 2, 3, 4, 5]) # one-dim array
 copy = array.copy() # creates a copy of og array
-array[0] = 10
+array[0] = 10 # changes the og array
 print(array)
+print(copy) # but no changes are made to the copy
 
+# View
+array2 = np.array([1, 2, 3, 4, 5]) # one-dim array
+view = array2.view() # creates a copy of og array
+array2[0] = 10
+print(array2) # prints updated version of og array
+print(view) # view shows the updated version of og array
+# if you do view[0] = 10, will also change the original array2
 
-
+# Python to recall whether it's a copy or a view
+print(view.base) # will return array (with changes)
+print(copy.base) # will return None
